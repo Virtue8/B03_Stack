49e105a6d4ee01ab58264f1755491db2afde5b38
Virtue_8 <virtue_8@mail.ru>
2024-10-04 18:47:39 +0300

    Push, Pop, Ctor, Dtor upgraded + Dump

--- stack.cpp
+++ stack.cpp
@@ -8,11 +8,11 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-#define LEGITCHECK if (err!=0) err =
+#define LEGITCHECK if (err!=1) err =
 
 //--------------------------------------------------------//
 
-typedef double StackElements_t;
+typedef long StackElements_t;
 
 struct Stack
 {
@@ -33,11 +33,11 @@ enum ErrorCodes
 
 int StackPush   (struct Stack * stk, StackElements_t unit);         //----|
 int StackPop    (struct Stack * stk);                               //    |
-int StackCtor   (struct Stack * stk, size_t cap);                   //    |
-int StackDtor   ();                                                 //    |----- Standart Stack Functions Stack
+int StackCtor   (struct Stack * stk);                   //    |
+int StackDtor   (struct Stack * stk);                               //    |----- Standart Stack Functions Stack
 int StackDump   (struct Stack * stk);                               //    |
-int StackOK     ();                                                 //    |
-int StackError  (struct Stack * stk);                               //----|
+//int StackOK     ();                                               //    |
+//int StackError  (struct Stack * stk);                             //----|
 
 
 //--------------------------------------------------------//
@@ -47,46 +47,56 @@ int main ()
     int err = 0;
     Stack stk = {};
 
-    LEGITCHECK StackCtor (&stk, 10);
+    LEGITCHECK StackCtor (&stk);
     LEGITCHECK StackPush (&stk, 10);
     LEGITCHECK StackPush (&stk, 20);
     LEGITCHECK StackPush (&stk, 30);
     LEGITCHECK StackPush (&stk, 40);
     LEGITCHECK StackPush (&stk, 50);
+    LEGITCHECK StackPop  (&stk);
+    LEGITCHECK StackPush (&stk, 60);
+    LEGITCHECK StackPush (&stk, 70);
+    LEGITCHECK StackDump (&stk);
 }
 
 //--------------------------------------------------------//
 
-int StackCtor (struct Stack * stk, size_t cap)     //return error type;
+int StackCtor (struct Stack * stk)     //return error type;
 {
     //verification                 switch(if) zero stack ewh
     stk->Size = 0;
-    stk->Capacity = cap;
+    stk->Capacity = 1;
     stk->Data = (StackElements_t *) calloc (stk->Capacity, sizeof(StackElements_t));
     //verification
-    return 1;
+    return 0;
 }
 
 //--------------------------------------------------------//
 
 int StackPush (struct Stack * stk, StackElements_t unit)
 {
-    stk->Capacity = stk->Size*2;
-    stk->Data = (StackElements_t *) realloc (stk->Data, stk->Capacity);
-    stk->Data[stk->Size - 1] = unit;
+    stk->Data[stk->Size] = unit;
+    printf ("element data[%lu] = %ld pushed\n", stk->Size, stk->Data[stk->Size]);
+    stk->Size++;
+    stk->Capacity = stk->Size * 2;
+    stk->Data = (StackElements_t *) realloc (stk->Data, stk->Capacity*sizeof(StackElements_t));
 
-    return 1;
+    return 0;
 }
 
 //--------------------------------------------------------//
 
 int StackPop (struct Stack * stk)
 {
-    stk->Capacity = stk->Size*2;
-    stk->Data = (StackElements_t *) realloc (stk->Data, stk->Capacity);
-    stk->Data[stk->Size - 1] = 0;
+    printf ("The last element '%ld' is deleted\n", stk->Data[stk->Size-1]);
 
-    return 1;
+    stk->Data[stk->Size] = 0;
+    stk->Size--;
+
+    stk->Capacity = stk->Size * 2;
+    stk->Data = (StackElements_t *) realloc (stk->Data, stk->Capacity*sizeof(StackElements_t));
+
+    return 0;
 }
 
 //--------------------------------------------------------//
@@ -98,27 +108,30 @@ int StackDtor (struct Stack * stk)
     stk->Size = 0;
     stk->ErrorCode = 0;
 
-    return 1;
+    return 0;
 }
 
 //--------------------------------------------------------//
-
+/*
 int StackError (struct Stack * stk)
 {
 
 }
-
+*/
 //--------------------------------------------------------//
 
 int StackDump (struct Stack * stk)
 {
+    printf ("\n\n");
     printf ("----------------------\n"
             "| Stack State Report |\n"
             "----------------------\n\n");
 
-    printf ("Error code %d: s", stk->ErrorCode);
+    printf ("Error code: %d \n", stk->ErrorCode);
     printf ("----------------------\n");
-    printf ("Stack data size: %d\n", stk->Size);
-    printf ("Stack capacity:  %d\n", stk->Capacity);
+    printf ("Stack data size: %lu\n", stk->Size);
+    printf ("Stack capacity:  %lu\n", stk->Capacity);
     printf ("----------------------\n");
+
+    return 0;
 }
